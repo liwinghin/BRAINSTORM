72fad8a53ab2bee20c525ad807f21c4290a81f3c
liwinghin <whlimichael1214@gmail.com>
2024-04-19 15:53:09 +0900

    Path Finding

---
+++ Assets/Scripts/PathFinding/MapGenerator.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+using System.Collections.Generic;
+
+namespace PathFinding
+{
+    public class MapGenerator : MonoBehaviour
+    {
+        [SerializeField] Tilemap tilemap;
+        [SerializeField] TileBase[] tiles; // 你的瓦片集
+
+        [SerializeField] int width = 6;
+        [SerializeField] int height = 6;
+        struct TileInfo
+        {
+            public Vector3Int position;
+            public string coordinate;
+
+            public TileInfo(Vector3Int pos, string coord)
+            {
+                position = pos;
+                coordinate = coord;
+            }
+        }
+
+        List<TileInfo> tileInfoList = new List<TileInfo>();
+
+
+        void Start()
+        {
+            GenerateMap();
+        }
+
+        void GenerateMap()
+        {
+            GameObject colliderParent = new GameObject("TileColliders");
+
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    Vector3Int tilePosition = new Vector3Int(x, y, 0);
+                    int tileIndex = Random.Range(0, tiles.Length);
+                    TileBase tile = tiles[tileIndex];
+                    tilemap.SetTile(tilePosition, tile);
+
+                    GameObject tileGameObject = new GameObject("TileCollider");
+                    tileGameObject.transform.position = tilemap.GetCellCenterWorld(tilePosition);
+                    tileGameObject.AddComponent<BoxCollider2D>().isTrigger = true;
+                    tileGameObject.transform.parent = colliderParent.transform;
+                    tileGameObject.tag = "Tile";
+
+                    string coordinate = "(" + x + ", " + y + ")";
+                    tileInfoList.Add(new TileInfo(tilePosition, coordinate));
+                }
+            }
+        }
+
+        public string GetCoordinateFromPosition(Vector3Int position)
+        {
+            foreach (TileInfo tileInfo in tileInfoList)
+            {
+                if (tileInfo.position == position)
+                {
+                    return tileInfo.coordinate;
+                }
+            }
+            return "Unknown";
+        }
+    }
+}
---
+++ Assets/Scripts/PathFinding/TileHighlighter.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using UniRx;
+
+namespace PathFinding
+{
+    public class TileHighlighter : MonoBehaviour
+    {
+        public Tilemap tilemap;
+
+        [SerializeField] Vector3Int offset = new Vector3Int(1, 1, -1);
+
+        [SerializeField] TileBase defaultTile;
+        [SerializeField] TileBase hoverTile;
+        [SerializeField] TileBase pointTile;
+
+        [SerializeField] ReactiveProperty<Vector3Int> hoveredTilePosition = new ReactiveProperty<Vector3Int>(new Vector3Int(-1, -1, -1));
+
+        void Start()
+        {
+            hoveredTilePosition
+                .Pairwise()
+                .Subscribe(pair =>
+                {
+                    SetTile(pair.Previous, defaultTile);
+                    SetTile(pair.Current, hoverTile);
+                })
+                .AddTo(this);
+
+            Observable.EveryUpdate()
+                .Select(_ => GetHoveredTilePosition())
+                .DistinctUntilChanged()
+                .Subscribe(tilePosition => hoveredTilePosition.Value = tilePosition)
+                .AddTo(this);
+
+            Observable.EveryUpdate()
+                .Where(_ => Input.GetMouseButtonDown(0)) // 检测鼠标左键点击
+                .Select(_ => GetClickedTilePosition())
+                .Where(tilePosition => IsValidTilePosition(tilePosition))
+                .Subscribe(tilePosition => tilemap.SetTile(tilePosition + offset, pointTile))
+                .AddTo(this);
+        }
+
+        Vector3Int GetHoveredTilePosition()
+        {
+            RaycastHit2D hit = Physics2D.GetRayIntersection(Camera.main.ScreenPointToRay(Input.mousePosition));
+            if (hit.collider != null && hit.collider.gameObject.CompareTag("Tile"))
+            {
+                return tilemap.WorldToCell(hit.point);
+            }
+            else
+            {
+                return new Vector3Int(-1, -1, -1);
+            }
+        }
+
+        void SetTile(Vector3Int tilePosition, TileBase tile)
+        {
+            if (tilePosition.x >= 0 && tilePosition.y >= 0 && tilePosition.z >= 0)
+            {
+                tilemap.SetTile(tilePosition, tile);
+            }
+        }
+        Vector3Int GetClickedTilePosition()
+        {
+            return hoveredTilePosition.Value;
+        }
+
+        bool IsValidTilePosition(Vector3Int tilePosition)
+        {
+            return tilePosition.x >= 0 && tilePosition.y >= 0 && tilePosition.z >= 0;
+        }
+    }
+}
