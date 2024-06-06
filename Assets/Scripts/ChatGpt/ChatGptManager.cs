0d7db90fd8dbeeb3ede9af86a4a314d7cf12a7da
liwinghin <whlimichael1214@gmail.com>
2024-06-06 13:15:44 +0900

    Networking Test

--- Assets/Scripts/ChatGpt/ChatGptManager.cs
+++ Assets/Scripts/ChatGpt/ChatGptManager.cs
@@ -1,8 +1,6 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using OpenAI;
-using UnityEditor.PackageManager.Requests;
 using UnityEngine.Events;
 
 public class ChatGptManager : MonoBehaviour
---
+++ Assets/Scripts/Networking (Mirror)/ChatRoomCore.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using Mirror;
+using UniRx;
+using TMPro;
+
+public class ChatRoomCore : NetworkBehaviour
+{
+    public TextMeshProUGUI chatLog;
+    public TMP_InputField chatInput;
+
+    public void Start()
+    {
+        chatLog = GameObject.Find("ChatLog").GetComponent<TextMeshProUGUI>();
+        chatInput = GameObject.Find("ChatInput").GetComponent<TMP_InputField>();
+
+        if (isLocalPlayer)
+        {
+            chatInput.onEndEdit.AsObservable()
+                    .Where(_ => Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.KeypadEnter))
+                    .Subscribe(_ => OnSendMessage())
+                    .AddTo(this);
+        }
+    }
+
+
+    private void OnSendMessage()
+    {
+        if (string.IsNullOrEmpty(chatInput.text)) return;
+
+        CmdSendMessage(chatInput.text);
+        chatInput.text = string.Empty;
+    }
+
+    [Command]
+    private void CmdSendMessage(string message)
+    {
+        RpcReceiveMessage(message);
+    }
+
+    [ClientRpc]
+    private void RpcReceiveMessage(string message)
+    {
+        chatLog.text += message + "\n";
+    }
+}
